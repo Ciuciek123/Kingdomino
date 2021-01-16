96a577f5e328fd1c1f193c269e6bd1fbc34ad9af
DESKTOP-NKH8UAO\marci <marcin.ciucmanski@gmail.com>
2021-01-16 21:59:13 +0100

    Image in MenuScreen

--- core/src/screens/MenuScreen.java
+++ core/src/screens/MenuScreen.java
@@ -1,11 +1,24 @@
 package screens;
 
+import com.badlogic.gdx.graphics.Texture;
 import com.mygdx.game.Kingdomino;
 
 public class MenuScreen extends AbstractScreen{
 
+    private Texture menuImg;
     public MenuScreen(Kingdomino game) {
         super(game);
+        init();
+    }
+    private void init(){
+        menuImg = new Texture("menu.jpg");
     }
 
+    @Override
+    public void render(float delta) {
+        super.render(delta);
+        spriteBatch.begin();
+        spriteBatch.draw(menuImg,0,0);
+        spriteBatch.end();
+    }
 }
