8f2c62d24eb7c80d13841f6606ddba8f97bba59b
DESKTOP-NKH8UAO\marci <marcin.ciucmanski@gmail.com>
2021-01-16 21:45:02 +0100

    Added dummy MenuScreen

---
+++ core/src/screens/MenuScreen.java
@@ -0,0 +1,11 @@
+package screens;
+
+import com.mygdx.game.Kingdomino;
+
+public class MenuScreen extends AbstractScreen{
+
+    public MenuScreen(Kingdomino game) {
+        super(game);
+    }
+
+}
