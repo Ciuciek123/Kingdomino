542810e670ac7261615331d3d296aabaf881e9cd
DESKTOP-NKH8UAO\marci <marcin.ciucmanski@gmail.com>
2021-02-07 19:36:13 +0100

    add Game class

--- core/src/Engine/Game.java
+++ core/src/Engine/Game.java
@@ -5,11 +5,20 @@ enum Token{
     P2
 }
 public class Game {
+    private Player p1;
+    private Player p2;
+    private Token playerToken;
     public Game(){
         init();
     }
 
     private void init() {
-
+        playerToken = Token.P1;
+    }
+    public void changePlayer(){
+        if(playerToken == Token.P1) {
+            playerToken=Token.P2;
+        } else
+            playerToken=Token.P1;
     }
 }
--- core/src/Engine/Player.java
+++ core/src/Engine/Player.java
@@ -1,11 +1,12 @@
 package Engine;
 
+import com.badlogic.gdx.scenes.scene2d.Actor;
 import playArea.PlayArea;
 
-public class Player {
+public class Player{
     int points = 0;
     PlayArea playerArea = new PlayArea();
     public Player() {
-        
+
     }
 }
